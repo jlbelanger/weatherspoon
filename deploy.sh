2638670de9fccdf4bf7c6518e74ff2dc25755af2
Jenny Belanger <jbelangr@gmail.com>
2025-08-08 21:54:06 -0400

    Update deploy script

--- deploy.sh
+++ deploy.sh
@@ -4,6 +4,7 @@ set -e
 APP_NAME=$(basename "${PWD}")
 
 source "${HOME}/Websites/infrastructure/deploy/config.sh"
+source "${HOME}/Websites/infrastructure/deploy/composer.sh"
 source "${HOME}/Websites/infrastructure/deploy/etc.sh"
 source "${HOME}/Websites/infrastructure/deploy/git.sh"
 source "${HOME}/Websites/infrastructure/deploy/static.sh"
@@ -11,5 +12,8 @@ source "${HOME}/Websites/infrastructure/deploy/static.sh"
 check_git_branch
 build_static
 check_git_changes
+deploy_git
 deploy_static
+deploy_env
+deploy_composer
 printf "\e[0;32mDone.\n\e[0m"
